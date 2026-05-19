3aa7a2cef66e0a4e617e404b71408b8b653fd818
rama918 <vaddiramadevi20@gmail.com>
2026-05-19 19:15:24 +0530

    special vars

--- conditions.sh
+++ conditions.sh
@@ -2,13 +2,13 @@
 
 NUMBER=$1
 
--gt --> greater than
--lt --> less than
--eq --> equal to
--ne --> not equal to
+# -gt --> greater than
+# -lt --> less than
+# -eq --> equal to
+# -ne --> not equal to
 
 
-if  [ NUMBER -lt 10 ]
+if  [ $NUMBER -lt 10 ]
 then
 echo "Given number $NUMBER is less than 10"
 else
