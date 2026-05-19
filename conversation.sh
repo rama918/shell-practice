3387263caa627a0bce4a086ce6f9bafb35634dec
rama918 <vaddiramadevi20@gmail.com>
2026-05-19 13:47:22 +0530

    hello world created

---
+++ conversation.sh
@@ -0,0 +1,7 @@
+#!/bin/bash/
+
+echo "Trump:: Hey Musk, how are you?"
+echo "Musk:: I am doing great, thanks for asking!"
+echo "Trump: I am free too. What's up?"
+echo "Musk:: Nothing,just going to mars now,will yo come"
+echo "Trump:: Sorry, you can carry on! I will come once you come back"
