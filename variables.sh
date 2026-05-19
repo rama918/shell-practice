85704032e1cf1a393dddac479e3c492a91bcf8fa
rama918 <vaddiramadevi20@gmail.com>
2026-05-19 13:57:29 +0530

    hello world created

---
+++ variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash/
+
+PERSON1=Trump
+PERSON2=Musk
+
+echo "$PERSON1:: Hey $PERSON2, how are you?"
+echo "$PERSON2:: I am doing great, thanks for asking!"
+echo "$PERSON1:: I am free too. What's up?"
+echo "$PERSON2:: Nothing,just going to mars now,will yo come"
+echo "$PERSON1:: Sorry, you can carry on! I will come once you come back"
