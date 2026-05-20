2d7aca7d42caa18c20a41b63230dac943dad867d
rama918 <vaddiramadevi20@gmail.com>
2026-05-20 13:43:46 +0530

    loops

---
+++ loops.sh
@@ -0,0 +1,47 @@
+#!/bin/bash/
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[31m"
+N="\e[31m"
+LOGS_FOLDER="/var/log/shellscript-logs"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+PACKAGES=("mysql" "python" nginx" httpd")
+mkdir -p $LOGS_FOLDER
+echo "Script strated executed at: $(date)" | tee -a $LOG_FILE
+
+if [ $USERID -ne 0 ] 
+then
+    echo -e "$R ERROR: please run this script with root access" | tee -a $LOG_FILE
+    exit 1
+else
+    echo "You are running with root access" | tee -a $LOG_FILE
+    fi
+
+# validate functions takes input as exit status, what command they try to install
+    VALIDTAE(){
+    if [ $1 -eq 0 ]
+then
+    echo -e "Installing $2 is ... $G SUCCESS $N" | tee -a $LOG_FILE
+else
+    echo -e "Installing $2 is ... $G FAILURE $N" | tee -a $LOG_FILE
+    
+fi
+    }
+
+for package in ${PACKAGES[@]}
+DO
+dnf list installed package &>>$LOG_FILE
+ if [ $? -ne 0 ]
+then
+    echo "package is not installed... going to install it" | tee -a $LOG_FILE
+    dnf install package -Y &>>$LOG_FILE
+    VALIDATE $? ""
+else
+    echo -e "nothing to do package... $Y already installed $N " | tee -a $LOG_FILE
+    
+fi
+done
+ 
