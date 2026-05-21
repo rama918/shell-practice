1b233c30ac7ac1e4cd3780d9e4c60d1b37667fc3
rama918 <vaddiramadevi20@gmail.com>
2026-05-21 12:41:47 +0530

    loops

---
+++ script-1.sh
@@ -0,0 +1,8 @@
+#!/bin/bash/
+
+GREETING="Hello,Good Morning"
+
+echo "$GREETING"
+echo "PID of the script-1 is: $$"
+
+./script-2.sh
---
+++ script-2.sh
@@ -0,0 +1,5 @@
+#!bin/bash/
+
+echo "PID of the script-2 is: $$"
+
+echo "$Greeting"
